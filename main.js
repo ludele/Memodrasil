6c7eedd0cc789adbbce43be92f772b24e6e0165f
Ludvig Johansson <ludvig.johansson1@learnet.se>
2023-09-12 15:54:53 +0200

    simple foundation added

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-use strict;
+"use strict";
+
+// Node.js module 'fs' needed for file access  
+var fs = require('fs');
+
+console.log("Hello, World!");
+
+function addEntry() {
+// Example foundation    
+
+}
+
+function viewEntries() {
+// Example foundation    
+
+}
+
+function commandLineMenu() {
+// Example foundation    
+
+}
+
+function createJSON() {
+// Example foundation    
+
+}
+
+function searchJSON() {
+// Example foundation    
+
+}
